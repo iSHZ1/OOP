4bdb4725262ac7b1154f0a33452a2cbf49d3c639
ISHZ <Xardas366@gmail.com>
2022-04-04 22:57:15 +0300

    Finished
    Finished

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = 3.556;
+            double b = 5.555;
+            RationalNumber a1 = new RationalNumber(a);
+            RationalNumber b1 = new RationalNumber(b);
+
+            a1.PrintInfo();
+            Console.WriteLine(a1.ToString()); 
+            a1.PrintInfo();
+
+            if (a1 > b1)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("Not");
+            }
+
+            RationalNumber c1 = a1 + b1;
+
+            c1.PrintInfo();
+
+            c1 = a1 * b1;
+
+            c1.PrintInfo();
+            c1.ToString();
+        }
+    }
+}
